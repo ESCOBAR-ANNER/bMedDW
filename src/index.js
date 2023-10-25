cadf5d26f42f05db21e48f972ced549e284aab37
ESCOBAR-ANNER <aescobarc13@miumg.edu.gt>
2023-10-25 04:50:02 -0600

    updatin, get y post funcionando

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express =require('express')
 const app = express();
 require('./database');
-const cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 //convertir datos que recibe el servidor en json
+
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/userRoutes'));
@@ -13,4 +15,3 @@ app.use(require('./routes/userRoutes'));
 
 app.listen(4000);
 console.log("Server on port", 4000);
-
